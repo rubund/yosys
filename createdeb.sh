70d2c627285f090a6f0604d9b7b4c0e0b5ccbc68
Ruben Undheim <ruben.undheim@gmail.com>
2014-04-12 20:05:43 +0200

    Added script for generating deb file

---
+++ createdeb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PACKAGENAME=yosys
+VERSION=0.2.0+
+rm -rf createdeb/
+mkdir createdeb
+mkdir createdeb/$PACKAGENAME-$VERSION
+git archive master | tar -x -C createdeb/$PACKAGENAME-$VERSION
+cd createdeb
+tar -czvf $PACKAGENAME\_$VERSION.orig.tar.gz $PACKAGENAME-$VERSION
+cp -r -v ../debian $PACKAGENAME-$VERSION/
+cd $PACKAGENAME-$VERSION
+dpkg-buildpackage
+#cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .
+#debuild
