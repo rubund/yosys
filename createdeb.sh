9a57de8d1332bac36d2f1e2a7c4de327840928d2
Ruben Undheim <ruben.undheim@gmail.com>
2014-05-17 00:55:13 +0200

    Updated version

--- createdeb.sh
+++ createdeb.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 PACKAGENAME=yosys
-VERSION=0.2.0+
+VERSION=0.2.0+-git1-68c0595
 rm -rf createdeb/
 mkdir createdeb
+#rm -rf createdeb/$PACKAGENAME-$VERSION
 mkdir createdeb/$PACKAGENAME-$VERSION
 git archive master | tar -x -C createdeb/$PACKAGENAME-$VERSION
 cd createdeb
 tar -czvf $PACKAGENAME\_$VERSION.orig.tar.gz $PACKAGENAME-$VERSION
+#tar -xvf $PACKAGENAME\_$VERSION.orig.tar.gz
 cp -r -v ../debian $PACKAGENAME-$VERSION/
 cd $PACKAGENAME-$VERSION
 dpkg-buildpackage
