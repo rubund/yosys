c3d0264ecdd5a963c46da0896ddbaae511ca8dd4
Ruben Undheim <ruben.undheim@gmail.com>
2018-10-18 21:10:14 +0200

    Feedback for tests

--- tests/svinterfaces/runtest.sh
+++ tests/svinterfaces/runtest.sh
@@ -1,25 +1,35 @@
 #!/bin/bash -e
 
 
+TESTNAME=$1
 
-$PWD/../../../yosys-min/yosys -p "read_verilog -sv svinterface1.sv ; hierarchy -check -top TopModule ; synth ; write_verilog svinterface1_syn.v"
-$PWD/../../../yosys-min/yosys -p "read_verilog -sv svinterface1_ref.v ; hierarchy -check -top TopModule ; synth ; write_verilog svinterface1_ref_syn.v"
+STDOUTFILE=${TESTNAME}.log_stdout
+STDERRFILE=${TESTNAME}.log_stderr
 
-iverilog -g2012 svinterface1_syn.v
-iverilog -g2012 svinterface1_ref_syn.v
+echo "" > $STDOUTFILE
+echo "" > $STDERRFILE
 
-iverilog -g2012 svinterface1_tb.v svinterface1_ref_syn.v
+echo -n "Test: ${TESTNAME} -> "
+
+$PWD/../../yosys -p "read_verilog -sv ${TESTNAME}.sv ; hierarchy -check -top TopModule ; synth ; write_verilog ${TESTNAME}_syn.v" >> $STDOUTFILE >> $STDERRFILE
+$PWD/../../yosys -p "read_verilog -sv ${TESTNAME}_ref.v ; hierarchy -check -top TopModule ; synth ; write_verilog ${TESTNAME}_ref_syn.v" >> $STDOUTFILE >> $STDERRFILE
+
+iverilog -g2012 ${TESTNAME}_syn.v
+iverilog -g2012 ${TESTNAME}_ref_syn.v
+
+iverilog -g2012 ${TESTNAME}_tb.v ${TESTNAME}_ref_syn.v
 ./a.out
 mv output.txt reference_result.txt
-iverilog -g2012 svinterface1_tb.v svinterface1_syn.v
+iverilog -g2012 ${TESTNAME}_tb.v ${TESTNAME}_syn.v
 ./a.out
 mv output.txt dut_result.txt
 
 diff reference_result.txt dut_result.txt
 RET=$?
 if [ "$RET" != "0" ] ; then
-    print "Failed"
+    print "failed"
     exit -1
 fi
 
+echo "ok"
 exit 0
